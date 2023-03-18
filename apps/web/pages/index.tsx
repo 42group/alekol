475869e8c901bae65e6d6c89abd55957085375b0
Valentin <valentin.furmanek@gmail.com>
2023-03-18 12:06:32 +0100

    feat(web): add the real index page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,25 @@
+import { Button, Separator } from '@alekol/shared/ui';
+import styles from '../styles/page-index.module.scss';
+
 export function Index() {
-  return <h1>Web</h1>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.description}>
+        To get started, login using the service of your choice.
+      </p>
+      <div className={styles.separator}>
+        <Separator />
+      </div>
+      <div className={styles.buttons}>
+        <Button color="discord" width="100%">
+          Login with Discord
+        </Button>
+        <Button color="ft" width="100%">
+          Login with 42
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
