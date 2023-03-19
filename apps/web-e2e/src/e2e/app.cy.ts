fde54bd3bafb9541e1f2c24168177c56125508ac
Valentin <valentin.furmanek@gmail.com>
2023-03-19 09:50:37 +0100

    test(web): add basic e2e tests

--- apps/web-e2e/src/e2e/app.cy.ts
+++ apps/web-e2e/src/e2e/app.cy.ts
@@ -1,9 +1,8 @@
-import { getGreeting } from '../support/app.po';
-
-describe('web', () => {
+describe('page /', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Web');
+  it('should display two buttons to log in', () => {
+    cy.get('button').contains('Login with Discord');
+    cy.get('button').contains('Login with 42');
   });
 });
