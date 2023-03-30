589b76a8d7ad600226021b6e8956773fbe185121
Valentin <valentin.furmanek@gmail.com>
2023-03-30 19:23:36 +0200

    feat(web): warn instead of throw in case of missing env variable

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,7 +1,9 @@
 if (!process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID)
-  throw "'NEXT_PUBLIC_DISCORD_CLIENT_ID' environment variable must be defined";
+  console.warn(
+    "'NEXT_PUBLIC_DISCORD_CLIENT_ID' environment variable must be defined"
+  );
 if (!process.env.NEXT_PUBLIC_BASE_URL)
-  throw "'NEXT_PUBLIC_BASE_URL' environment variable must be defined";
+  console.warn("'NEXT_PUBLIC_BASE_URL' environment variable must be defined");
 
 const config = {
   discord: {
