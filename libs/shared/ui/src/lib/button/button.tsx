4d48c2fd0e096b0851af001b6e10986eceb0056a
valfur03 <valentin.furmanek@gmail.com>
2023-04-01 12:17:30 +0200

    feat(ui): add disabled button

--- libs/shared/ui/src/lib/button/button.tsx
+++ libs/shared/ui/src/lib/button/button.tsx
@@ -4,6 +4,7 @@ import styles from './button.module.scss';
 export interface ButtonProps {
   children?: string;
   color?: 'primary' | 'secondary' | 'danger' | 'discord' | 'ft';
+  disabled?: boolean;
   href?: string;
   size?: 'small' | 'large';
   width?: string;
@@ -12,12 +13,15 @@ export interface ButtonProps {
 export function Button({
   children = 'Button',
   color = 'primary',
+  disabled = false,
   href,
   size = 'large',
   width = 'auto',
 }: ButtonProps) {
   const globalProps = {
-    className: `${styles.container} ${styles[color]} ${styles[size]}`,
+    className: `${styles.container} ${styles[color]} ${styles[size]} ${
+      disabled ? styles.disabled : ''
+    }`,
     style: { width },
   };
   if (href) {
