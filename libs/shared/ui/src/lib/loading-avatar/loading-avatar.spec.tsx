3cea89dd2c91be408b8b5a48a84eef1792f2b779
valfur03 <valentin.furmanek@gmail.com>
2023-04-01 13:25:11 +0200

    test(ui): add tests to LoadingAvatar component

--- libs/shared/ui/src/lib/loading-avatar/loading-avatar.spec.tsx
+++ libs/shared/ui/src/lib/loading-avatar/loading-avatar.spec.tsx
@@ -1,10 +1,28 @@
-import { render } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import { render, screen } from '@testing-library/react';
 
 import LoadingAvatar from './loading-avatar';
 
+const avatarUrl = faker.image.avatar();
+
 describe('LoadingAvatar', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<LoadingAvatar />);
-    expect(baseElement).toBeTruthy();
+  describe('when loading', () => {
+    it('should render successfully', () => {
+      render(<LoadingAvatar loading={true} />);
+      const avatarElement = screen.getByTestId('skeleton-wrapper');
+      expect(avatarElement).toBeInTheDocument();
+    });
+  });
+
+  describe('when not loading', () => {
+    it('should render successfully', () => {
+      render(<LoadingAvatar src={avatarUrl} />);
+      const avatarElement = screen.getByRole('img');
+      expect(avatarElement).toBeInTheDocument();
+      expect(avatarElement).toHaveProperty(
+        'src',
+        expect.stringContaining(encodeURIComponent(avatarUrl))
+      );
+    });
   });
 });
