9617f9b5d1f97c405de9f73ab3a3d538e0ca894c
valfur03 <valentin.furmanek@gmail.com>
2023-04-07 16:14:04 +0200

    chore(web): add environment variables to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  env: {
+    SESSION_PASSWORD: process.env.SESSION_PASSWORD,
+  },
   images: {
     remotePatterns: [
       {
