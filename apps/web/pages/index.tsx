10aec348778394f371383ae4b5b02c64a71466ba
valfur03 <valentin.furmanek@gmail.com>
2023-04-10 19:32:55 +0200

    feat(web): redirect to '/auth' if the user has already signed in with Discord

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,33 @@
+import { withIronSessionSsr } from 'iron-session/next';
+import { User } from '@alekol/shared/interfaces';
 import { Button, DiscordOauth2Button, Separator } from '@alekol/shared/ui';
+import { ironSessionWrapper } from '@alekol/shared/utils';
 import styles from '../styles/page-index.module.scss';
 import config from '../lib/config';
 
-export function Index() {
+export const getServerSideProps = withIronSessionSsr(
+  ironSessionWrapper(),
+  config.ironSession
+);
+
+export interface IndexProps {
+  user?: User;
+}
+
+export function Index({ user }: IndexProps) {
+  if (!user) return null;
+
+  const discordButton = user.accountLinking.discord ? (
+    <Button href="/auth" color="discord" width="100%">
+      Login with Discord
+    </Button>
+  ) : (
+    <DiscordOauth2Button
+      clientId={config.discord.clientId}
+      redirectUri={config.discord.redirectUri}
+    />
+  );
+
   return (
     <div className={styles.container}>
       <p className={styles.description}>
@@ -12,10 +37,7 @@ export function Index() {
         <Separator />
       </div>
       <div className={styles.buttons}>
-        <DiscordOauth2Button
-          clientId={config.discord.clientId}
-          redirectUri={config.discord.redirectUri}
-        />
+        {discordButton}
         <Button color="ft" width="100%">
           Login with 42
         </Button>
