31d292f8d1498bf195be07130153e6e884744def
valfur03 <valentin.furmanek@gmail.com>
2023-04-14 18:15:27 +0200

    feat(api): add 42 config

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -7,6 +7,14 @@ export default () => ({
       redirectUri: `${process.env.FRONTEND_BASE_URL}/auth/oauth2/discord/callback`,
     },
   },
+  ft: {
+    api: {
+      baseUrl: 'https://api.intra.42.fr/v2',
+      clientId: process.env.FT_API_CLIENT_ID,
+      clientSecret: process.env.FT_API_CLIENT_SECRET,
+      redirectUri: `${process.env.FRONTEND_BASE_URL}/auth/oauth2/42/callback`,
+    },
+  },
   ironSession: {
     cookieName: 'alekol_session',
     password: process.env.SESSION_PASSWORD,
