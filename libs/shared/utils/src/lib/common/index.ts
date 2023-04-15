26cf38a01146fa89a429618e94653f9bd011cafe
valfur03 <valentin.furmanek@gmail.com>
2023-04-15 09:25:10 +0200

    fix(utils): add method to generate 42 OAuth2 URL

--- libs/shared/utils/src/lib/common/index.ts
+++ libs/shared/utils/src/lib/common/index.ts
@@ -1,3 +1,4 @@
 export * from './generate-discord-oauth2-url';
 export * from './generate-discord-user-avatar-url';
+export * from './generate-ft-oauth2-url';
 export * from './iron-session-wrapper';
