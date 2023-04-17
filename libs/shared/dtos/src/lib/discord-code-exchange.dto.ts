502bf31d0d9b3d815ecf4cff877e0e89e4cb33ae
valfur03 <valentin.furmanek@gmail.com>
2023-04-17 18:46:31 +0200

    build: enable strict TS mode

--- libs/shared/dtos/src/lib/discord-code-exchange.dto.ts
+++ libs/shared/dtos/src/lib/discord-code-exchange.dto.ts
@@ -3,5 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class DiscordCodeExchangeDto {
   @IsNotEmpty()
   @IsString()
-  code: string;
+  code!: string;
 }
--- libs/shared/dtos/src/lib/ft-code-exchange.dto.ts
+++ libs/shared/dtos/src/lib/ft-code-exchange.dto.ts
@@ -3,5 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class FtCodeExchangeDto {
   @IsNotEmpty()
   @IsString()
-  code: string;
+  code!: string;
 }
