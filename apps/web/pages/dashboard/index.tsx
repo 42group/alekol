1c3a9c7478cc8c2eca943b78be6271eb8d0fcb4d
valfur03 <valentin.furmanek@gmail.com>
2023-04-20 10:38:08 +0200

    feat(web): add Dashboard page

---
+++ apps/web/pages/dashboard/index.tsx
@@ -0,0 +1,26 @@
+import { User } from '@alekol/shared/interfaces';
+import { DashboardHeader } from '@alekol/shared/ui';
+import { ironSessionWrapper } from '@alekol/shared/utils';
+import { withIronSessionSsr } from 'iron-session/next';
+import config from '../../lib/config';
+
+export const getServerSideProps = withIronSessionSsr(
+  ironSessionWrapper([(_, { user }) => !!user.id]),
+  config.ironSession
+);
+
+export interface DashboardProps {
+  user?: User;
+}
+
+export function Dashboard({ user }: DashboardProps) {
+  if (!user) return null;
+
+  return (
+    <>
+      <DashboardHeader />
+    </>
+  );
+}
+
+export default Dashboard;
