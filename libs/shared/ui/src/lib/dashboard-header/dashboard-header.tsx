1a0392d728122ad68772f1a773d77341cb0b23a8
valfur03 <valentin.furmanek@gmail.com>
2023-04-20 11:26:24 +0200

    feat(ui): add logout handler to DashboardHeader

--- libs/shared/ui/src/lib/dashboard-header/dashboard-header.tsx
+++ libs/shared/ui/src/lib/dashboard-header/dashboard-header.tsx
@@ -1,11 +1,32 @@
 import { Button } from '../button/button';
+import router from 'next/router';
 import styles from './dashboard-header.module.scss';
 
 export function DashboardHeader() {
+  const logoutOnClick = () => () => {
+    fetch('/api/auth/logout', { method: 'POST' })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw new Error(
+              error.message ||
+                'An unexpected error occured... Please try again.'
+            );
+          });
+        }
+      })
+      .catch(() => {
+        // nothing to do
+      });
+    return router.push('/');
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Button color="secondary">Logout</Button>
+        <Button color="secondary" onClick={logoutOnClick()}>
+          Logout
+        </Button>
       </div>
     </div>
   );
