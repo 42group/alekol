4d83d8774c6345076401f99e9a16daaffdbbb225
valfur03 <valentin.furmanek@gmail.com>
2023-04-23 13:03:03 +0200

    feat: add Redis

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from '../config';
 import { AuthModule } from '../auth/auth.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.getOrThrow<string>('cache.host'),
+        port: configService.getOrThrow<number>('cache.port'),
+        ttl: 0,
+        max: 0,
+      }),
+      inject: [ConfigService],
+      isGlobal: true,
+    }),
     ConfigModule.forRoot({
       load: [config],
     }),
