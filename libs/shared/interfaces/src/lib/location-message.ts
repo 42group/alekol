f91aa14cf0a5fbb6f1a81e204453949f511545a4
valfur03 <valentin.furmanek@gmail.com>
2023-04-24 15:11:53 +0200

    feat(interfaces): add id to LocationMessage

--- libs/shared/interfaces/src/lib/location-message.ts
+++ libs/shared/interfaces/src/lib/location-message.ts
@@ -2,6 +2,7 @@ export interface LocationMessage {
   identifier: string;
   message: {
     location: {
+      id: number;
       begin_at: string;
       end_at: string | null;
       host: string;
