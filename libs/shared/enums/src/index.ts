1b7a19cb2cdd90373a0843e1d44e4c95596a7b83
valfur03 <valentin.furmanek@gmail.com>
2023-05-05 12:26:38 +0200

    feat(enums): add UserRole

--- libs/shared/enums/src/index.ts
+++ libs/shared/enums/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/linkable-service';
 export * from './lib/authentication-status';
+export * from './lib/user-role';
---
+++ libs/shared/enums/src/lib/user-role.ts
@@ -0,0 +1,5 @@
+export enum UserRole {
+  None = 'none',
+  User = 'user',
+  Admin = 'admin',
+}
